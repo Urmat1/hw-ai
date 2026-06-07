b371d4ed2bc0c05ff7e3b441d36ec3f836e2e37c
Urmat1 <105582852+Urmat1@users.noreply.github.com>
2026-06-08 01:47:39 +0600

    Add files via upload

---
+++ int_float.py
@@ -0,0 +1,89 @@
+# # task 1
+# a = int(input("Enter an integer: "))
+# b = int(input("Enter another integer: "))
+# num1 = a + b or sum([a, b])
+# print(num1, type(num1))
+
+
+# task 2
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+# num2 = int(number1 / number2)
+# print(num2)
+
+# task 3
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+# num3 = number1 % number2
+# print(num3)
+
+# task 4
+# float_number = float(input("Enter a floating-point number: "))
+# float_number2 = float(input("Enter another floating-point number: "))
+# float_number3 = float(input("Enter a third floating-point number: "))
+# num4 = (float_number * float_number2) / float_number3
+# print(num4)
+
+# task 5
+# num = float(input("Enter a floating-point number: "))
+# num5 = round(num ** 3 / 2, 1)
+# print(num5)
+
+# task 6
+# import math
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# num6 = math.floor((num1 - num2) * 10) / 10
+# print(num6)
+
+# task 7
+# import math
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# num7 = math.ceil((num1 - num2) * 10) / 10
+# print(num7)
+
+# task 8
+# import math
+# a = float(input("Enter the first number: "))
+# b = float(input("Enter the second number: "))
+# c = math.sqrt(a**2 + b**2)
+# print(round(c, 1))
+
+# task 9
+# pos_num = 5
+# neg_num = -7
+# num9 = abs(pos_num) + abs(neg_num)
+# print(num9)
+
+# task 10
+# temp = 90
+# num10 = round((temp - 32) * 5 / 9, 2)
+# print(num10)
+
+# task 11
+# num11 = (((6 + 7/12) - (3 + 17/36)) * 2.5 - (4 + 1/3) / 0.65) / (4 / (1/4) - 0.5)
+# print(num11)
+
+# task 12
+# num12 = (((2 + 3/4) / 1.1 + (3 + 1/3)) / (2.5 - 0.4 * (3 + 1/3))) / (5/7) - (((2 + 1/6) + 4.5) * 0.375) / (2.75 - (1 + 1/2))
+# print(num12)
+
+# task 13
+# num13 = (11 + 2/5) + (7 + 1/2) * (285.6 / 14 - (1 + 23/30) + 13/50) / (24.4 - 10.23)
+# print(num13)
+
+# task 14
+# num14 = ((9 - (5 + 3/8)) * ((4 + 5/12) - 4 / (2 + 2/3)) + (3/10 - 0.5/4) * (4/7)) / (1/24 + 0.25 / (13 + 1/3))
+# print(num14)
+
+# task 15
+# num15 = (5.75 * 100)/2.5
+# print(num15)
+
+# task 16
+# import math
+# num16 = ((0.16 * (3.2 - 3/40) + (2 + 3/11) * 4.125 / (3 + 3/4)) / ((5 + 1/6) * 0.3 - 0.3 * 4.5 + (1/3) * 0.3)) * 0.4
+# print(math.ceil(num16))
+
+
