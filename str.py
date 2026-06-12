7916ab85cc88e72e34117768372ffe7358e41b4c
Urmat1 <105582852+Urmat1@users.noreply.github.com>
2026-06-12 20:13:21 +0600

    Add files via upload

---
+++ str.py
@@ -0,0 +1,84 @@
+# task 1
+# str1 = 'Hello'
+# str2 = 'world'
+# string1 = str1 + ' ' + str2
+# print(string1)
+
+# task 2 
+# str_ = input('text ')
+# string1 = [str_ for _ in range(3)]
+# print(string1)
+
+# task 3
+# string = 'Data Science is about working with big data''Data Science is about working with big data'
+# string3 = string.replace('a', '#')
+# print(string3)
+
+# task 4
+# name = 'Иван'
+# surname = 'Иванов'
+# string4 = name + ' ' + surname + ' ' + '-' ' ' + 'Отличный студент'
+# print(string4)
+
+# task 5
+# region_dic = {
+#     '01': 'Bishkek',
+#     '02': 'Osh',
+#     '03': 'Batken',
+#     '04': 'Jalal-Abad',
+#     '05': 'Naryn',
+#     '06': 'Osh region',
+#     '07': 'Talas',
+#     '08': 'Chuy',
+#     '09': 'Issyk-Kul'
+# }
+# number = input('Введите регион: ')
+# print(region_dic[number])
+
+# task 6
+# string6 = input('Write text ')
+
+# if any(x.isupper() for x in string6):
+#     print('uppercase_string')
+# elif any(x.islower() for x in string6):
+#     print('lowercase_string')
+# else:
+#     print('other')
+
+# task 7
+# string7 = input()
+
+# if any(y.isalpha() for y in string7):
+#     print('is_alpha')
+# elif any(y.isdigit() for y in string7):
+#     print('is_digit')
+# else:
+#     print('other')
+
+# task 8
+# string8 = input()
+# print(string8.startswith('Data Science'))
+
+# Or
+ 
+# string8 = input()
+# str = 'Data Science'
+# for x in string8:
+#     for y in str:
+#         if x in y:
+#             print(True)
+#         else:
+#             print(False)
+
+# task 9
+# string = input()
+# string9 = string[-1] + string[1:-1] + string[0]
+# print(string9)
+
+# task 10
+# string = input()
+# string10 = string[::-1]
+# print(string10)
+
+
+
