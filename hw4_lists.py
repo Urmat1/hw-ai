b7c437037916dc2e273f2ff0c9c2da06c630ef15
Urmat1 <105582852+Urmat1@users.noreply.github.com>
2026-06-27 15:44:51 +0600

    Add files via upload

---
+++ hw4_lists.py
@@ -0,0 +1,50 @@
+#task 1
+# list_ = [1, 2, 3, 4, 5, 6]
+# list1 = list_[2]
+# print(list1)
+
+#task 2 
+# list2 = [23, 34, 12, 7, 0, -4, -56, -100, 344, 98, 232]
+# print(sorted(list2))
+
+# task 3
+# list_ = [1, 22, 3, 45, 22, 4, 89, 87, 87, 4]
+# num = int(input("Number "))
+# list3 = list_.count(num)
+# print(list3)
+
+# task 4 
+# fruits = [
+#     "Яблоко",
+#     "Банан",
+#     "Апельсин",
+#     "Манго",
+#     "Киви",
+#     "Ананас",
+#     "Груша"
+# ]
+# list4 = fruits[:5]
+# print(list4)
+# list4[2] = fruits[6]
+# print(list4)
+
+# task 5
+# list5 = [list for list in range(0, 20)]
+# print(list5)
+
+# task 6
+# ls1 = [1, 2, 4, 8, 9]
+# ls2 = [3, 1, 5, 6, 0]
+# ls3 = ls1 + ls2
+# list6 = sum(ls3)
+# print(list6)
+
+# task 7
+# lst7 = [22, 1, 2, 4, 5 ,90, 87, 27]
+# list9 = list(filter(lambda x: x > 10, lst7))
+# print(list9)
+
+# task 8 
+# str_ = 'The second type of coordinate system is a projected coordinate system.'
+# list10 = str_.split()
+# print(list10)
