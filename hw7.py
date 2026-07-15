ce9d34e2f6215c66a2f30d8f91c0c3085f0cdd5d
Urmat1 <105582852+Urmat1@users.noreply.github.com>
2026-07-15 21:01:09 +0600

    Add files via upload

---
+++ hw7.py
@@ -0,0 +1,42 @@
+# task 1 
+# dict1 = {
+#     'apple': 'red',
+#     'melon': 'yellow',
+#     'banana': 'yellow'
+# }
+# dict1.clear()
+# print(dict1)
+
+# task 2
+# dict_ = {'a': 1, 'b': 2, 'c': 1}
+# print(dict_.keys())
+
+# task 3
+# dict_ = {'a': 1, 'b': 2, 'c': 1}
+# for a, b in dict_.items():
+#     print(a, b)
+
+# task 4
+# dict4 = {'a': 1, 'b': 4, 'c': 1, 'd': 5, 'e': 6}
+# for a, b in dict4.copy().items():
+#     if b < 4:
+#         del dict4[a]
+# print(dict4)
+
+# task 5
+# dict5 = {1: {'a': 1}, 2: {'b': 4}, 3: {'c': 1}, 4: {'d': 5}, 5: {'e': 6}}
+# for key, value in dict5.items():
+#     dict5[key] = list(value.keys())[0]
+# print(dict5)
+
+# task 7
+# dict7 = {'a': 1, 'b': 2, 'c': 1}
+# dict7['d'] =  3
+# print(dict7)
+
+# task 9
+# d1 = {'a': 1, 'b': 2}
+# d2 = {'c': 3, 'd': 4}
+# dict9 = d1.update(d2)
+# print(d1)
+
